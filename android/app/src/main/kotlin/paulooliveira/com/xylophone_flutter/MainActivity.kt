c70425ad7c7f5160024b35ad25176a45917d3c6f
pauloboliveira <pborges086@hotmail.com>
2021-05-28 21:20:41 -0300

    Subindo projeto

---
+++ android/app/src/main/kotlin/paulooliveira/com/xylophone_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package paulooliveira.com.xylophone_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+            child: Center(
+              child: FlatButton(
+                child: Text('Clique aqui'),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
