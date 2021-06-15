c33af3ab3c60472db0df35c287c69688cd2542a5
pauloboliveira <pborges086@hotmail.com>
2021-06-15 20:28:40 -0300

    Atualizando UI do app

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,37 @@ void main() => runApp(XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    void playSound(int soundNumber) {
+      final player = AudioCache();
+      player.play('note$soundNumber.wav');
+    }
+
+    buildKey(Color color, int soundNumber) {
+      return Expanded(
+        child: FlatButton(
+          color: color,
+          onPressed: () {
+            playSound(soundNumber);
+          },
+        ),
+      );
+    }
+
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(
-            child: Center(
-              child: FlatButton(
-                child: Text('Clique aqui'),
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note1.wav');
-                },
-              ),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.teal, 5),
+              buildKey(Colors.blue, 6),
+              buildKey(Colors.purple, 7),
+            ],
           ),
         ),
       ),
